32b347428401a79bc4df92ce195e72fe76c7a2b2
Antoine Reversat <a.reversat@gmail.com>
2015-04-24 14:09:44 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools.command.test import test as TestCommand
 
 setup(
     name='vcrwrapper',
-    version='0.0.2',
+    version='0.0.3',
     description=(
         "Wrapper around vcr"
     ),
